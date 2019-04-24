5d9edaf30feb679be7873641ce42926f42312126
RevatureGentry <william.gentry@revature.com>
2019-04-24 17:52:11 -0400

    Added Class.forName() to load JDBC driver

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -11,13 +11,18 @@ public class ConnectionFactory {
 	
 	public static Connection getConnection() {
 		try {
+			Class.forName("oracle.jdbc.OracleDriver");
 			return DriverManager.getConnection(System.getenv("JDBC_URL"), 
 											   System.getenv("JDBC_USERNAME"), 
 											   System.getenv("JDBC_PASSWORD"));
 		} catch (SQLException e) {
 			System.err.println("Error Code: " + e.getErrorCode());
 			System.err.println("SQL State: " + e.getSQLState());
+			System.err.println("Message: " + e.getMessage());
 			throw new RuntimeException("Failed to get database connection");
-		} 
+		} catch (ClassNotFoundException e) {
+			System.err.println("Failed to locate JDBC Driver");
+			throw new RuntimeException("Failed to locate JDBC driver");
+		}
 	}
 }
