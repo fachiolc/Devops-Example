0161dc167f571a5b6a6a904fd2c0c9913de440b1
RevatureGentry <william.gentry@revature.com>
2019-04-24 16:14:04 -0400

    Changed properties file to use Environment variables. Fixed Security group and sql table issues

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -1,38 +1,23 @@
 package com.revature.util;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionFactory {
 
-	private static final Properties props = getJdbcProperties();
-	
 	// Private Constructor to enforce usage of factory method
 	private ConnectionFactory() {}
 	
 	public static Connection getConnection() {
 		try {
-			return DriverManager.getConnection(props.getProperty("jdbc.url"), 
-					props.getProperty("jdbc.username"), 
-					props.getProperty("jdbc.password"));
+			return DriverManager.getConnection(System.getenv("JDBC_URL"), 
+											   System.getenv("JDBC_USERNAME"), 
+											   System.getenv("JDBC_PASSWORD"));
 		} catch (SQLException e) {
 			System.err.println("Error Code: " + e.getErrorCode());
 			System.err.println("SQL State: " + e.getSQLState());
 			throw new RuntimeException("Failed to get database connection");
 		} 
-	}
-	
-	private static Properties getJdbcProperties() {
-		Properties props = new Properties();
-		try {
-			props.load(Thread.currentThread()
-					.getContextClassLoader().getResourceAsStream("application.properties"));
-		} catch (IOException e) {
-			throw new RuntimeException("Failed to load application.properties");
-		} 
-		return props;
 	}
 }
